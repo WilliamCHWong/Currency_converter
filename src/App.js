44576a5a6e6d91d4f8f7f1d5f9b1006a077e2a36
William Cheuk Hei Wong <williamwongchurkhay@gmail.com>
2024-09-27 03:29:15 -0400

    Add option

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+const BASE_URL = "https://api.exchangeratesapi.io/v1/latest?access_key=e0adc88b3e732621961ad36507c6d00b&format=1"
+
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
+
+  useEffect(()=>{
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      })
+  }, [])
+
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow />
-      <div>=</div>
-      <CurrencyRow />
+      <CurrencyRow 
+        currencyOptions = {currencyOptions}  />
+      <div className='equals'>=</div>
+      <CurrencyRow 
+        currencyOptions = {currencyOptions} />
     </>
   );
 }
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function CurrencyRow() {
+export default function CurrencyRow(props) {
+    const{
+        currencyOptions
+    } = props
   return (
     <div>
-      Currency Row
+      <input type='number' className='input'/>
+      <select>
+        {currencyOptions.map(option => (
+            <option key={option} value={option}>{option}</option>
+        ))}
+      </select>
     </div>
   )
 }
